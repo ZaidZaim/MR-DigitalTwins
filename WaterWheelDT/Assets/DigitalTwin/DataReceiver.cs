f4e638c2c5a20358e9f72fcf7b777a9963c7ebb5
lucas martinic <42948357+lucas-martinic@users.noreply.github.com>
2021-11-20 13:18:07 +0100

    Imported naughty attributes

---
+++ WaterWheelDT/Assets/DigitalTwin/DataReceiver.cs
@@ -0,0 +1,16 @@
+using DigitalTwin;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DigitalTwin {
+    public class DataReceiver : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        public DigitalTwinData digitalTwinData;
+        public List<BottleItem> bottles;
+
+        public actio
+      
+    }
+}
